421658317bcadafada0fc7a017e1852712e9a01c
koki <koki.b2i4@gmail.com>
2021-06-21 07:13:01 +0900

    'DRY'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+	before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@tasks = Task.all
 	end
 
 	def show
-		@task = Task.find(params[:id])
 	end
 
 	def new
@@ -25,11 +26,9 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		@task = Task.find(params[:id])
 	end
 
 	def update
-		@task = Task.find(params[:id])
 
 		if @task.update(task_params)
 			flash[:success] = 'タスクが更新されました'
@@ -41,7 +40,6 @@ class TasksController < ApplicationController
 	end
 
 	def destroy
-		@task = Task.find(params[:id])
 		@task.destroy
 
 		flash[:success] = 'タスクが削除されました'
@@ -55,4 +53,8 @@ private
 # Strong Parameter
 def task_params
 	params.require(:task).permit(:content)
+end
+
+def set_task
+	@task = Task.find(params[:id])
 end
